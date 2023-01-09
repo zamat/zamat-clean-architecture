036e203cdd51d90240b6f4d21e966c36cd98d122
Mateusz Zawada <mateusz.zawada@asseco.pl>
2023-01-09 11:47:53 +0100

    Remove paginator common package

--- src/Commons/Zamat.Common.Paginator/PaginableResult.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Zamat.Common.Paginator;
-
-public record PaginableResult<TResult>(int CurrentPage = 1, int ItemsPerPage = 20, int TotalItems = 0)
-{
-    public int TotalPages => TotalItems > 0 ? (TotalItems % ItemsPerPage > 0 ? 1 : 0) + TotalItems / ItemsPerPage : 0;
-    public IEnumerable<TResult> Items { get; init; } = new List<TResult>();
-}

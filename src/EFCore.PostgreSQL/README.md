713f7d3280049bd1b2edb5e6849b7aab47349b30
Mateusz Zawada <mateusz.zawada@asseco.pl>
2024-01-11 09:16:41 +0100

    Migrate to net8.0

--- src/EFCore.PostgreSQL/README.md
+++ src/EFCore.PostgreSQL/README.md
@@ -1,3 +1,2 @@
 ﻿## Komendy pomocnicze
 dotnet ef migrations add CreateUsersDatabase --context UsersDbContext --output-dir Migrations/Users
-dotnet ef migrations add CreateContentDatabase --context ContentDbContext --output-dir Migrations/Content
--- src/Zamat.Clean.Cli/Program.cs
+++ src/Zamat.Clean.Cli/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
 using Zamat.Clean.Cli;
-using Zamat.Clean.Services.Content.Infrastructure;
 using Zamat.Clean.Services.Users.Infrastructure;
 
 var builder = ConsoleApp.CreateBuilder(args);
@@ -8,7 +7,6 @@ var builder = ConsoleApp.CreateBuilder(args);
 builder.ConfigureServices((host, services) =>
 {
     services.AddUsersDbContext(host.Configuration);
-    services.AddContentDbContext(host.Configuration);
 });
 
 builder.ConfigureLogging(configureLogging =>
